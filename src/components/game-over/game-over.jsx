923839207d6a032d52fe16ed5333d3d5e7a8868a
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2018-07-29 21:38:52 +0200

    Improve restart link

--- src/components/game-over/game-over.jsx
+++ src/components/game-over/game-over.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './game-over.css';
 
-const GameOver = (props) => (
-  <div className="GameOver">
-    You did it! Hit <a href="#null" onClick={props.restart}>Restart</a> to play again!
-  </div>
-)
+const GameOver = (props) => {
+  const restart = (e) => {
+    e.preventDefault();
+    props.restart();
+  }
+
+  return (
+    <div className="GameOver">
+      You did it! Hit <a href="/" onClick={restart}>Restart</a> to play again!
+    </div>
+  );
+}
 
 export default GameOver;
