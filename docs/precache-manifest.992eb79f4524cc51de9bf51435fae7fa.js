eb2dc499aad3baf542f88bba0d44e20b9a1deb38
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2019-06-28 09:25:26 +0200

    Add production build for github pages

---
+++ docs/precache-manifest.992eb79f4524cc51de9bf51435fae7fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6329819937310b53e1efb7950aa8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05317ef9c0627570208c",
+    "url": "/static/css/main.1e71dcd2.chunk.css"
+  },
+  {
+    "revision": "38c21d790a935b336fe6",
+    "url": "/static/js/2.eb42188a.chunk.js"
+  },
+  {
+    "revision": "05317ef9c0627570208c",
+    "url": "/static/js/main.f0372aef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
